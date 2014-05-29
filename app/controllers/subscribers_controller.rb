04d142de9367558946bb2d34b194ef44e809b692
Alex Frost <alex.frost.dev@gmail.com>
2014-05-29 16:27:52 +0100

    add devise views and add more subscriber fields

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,5 +1,6 @@
 class SubscribersController < ApplicationController
   before_action :set_subscriber, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_subscriber!
 
   # GET /subscribers
   # GET /subscribers.json
