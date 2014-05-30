06a0d2663076ffe4a0654b56f8834197492d5ca4
Alex Frost <alex.frost.dev@gmail.com>
2014-05-30 23:48:24 +0100

    use selenium driver for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/poltergeist' 
-Capybara.javascript_driver = :poltergeist
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :selenium
+Capybara.current_driver = :selenium
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -29,7 +30,22 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before :each do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
