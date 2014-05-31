61bd0c0826fa826ff526791c86fa6d9a4c0410d0
Alex Frost <alex.frost.dev@gmail.com>
2014-05-31 09:22:18 +0100

    add letter_opener and host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+
   Pony.options = {
     :via => :smtp,
     :via_options => {
