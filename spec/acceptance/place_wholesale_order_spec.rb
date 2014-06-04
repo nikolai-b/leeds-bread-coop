0fd8239399b6c6d8821e9aa5ab681466a9fa8ea8
Alex Frost <alex.frost.dev@gmail.com>
2014-06-04 17:03:28 +0100

    update place_wholesale_order_spec

--- spec/acceptance/place_wholesale_order_spec.rb
+++ spec/acceptance/place_wholesale_order_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-feature "New user, new sign-up", type: :feature,  js: false  do
+feature "New user, new sign-up", type: :feature,  js: true  do
   before :each do
-    create :bread_type
+    Capybara.javascript_driver = :poltergeist
+    Capybara.current_driver = :poltergeist
     create :wholesale_customer
     create :subscriber, :admin
   end
@@ -10,12 +11,17 @@ feature "New user, new sign-up", type: :feature,  js: false  do
   scenario "Place a customer order" do
     sign_in_as_admin
 
-    click_on "Orders"
+    click_on "Wholesale"
 
     click_on "New Order"
 
+    expect(page).to have_content "Lanes"
+
     fill_in_order_details
 
+    expect(page).to have_content "Order was successfully created."
+
+    expect(page).to have_content "White sour: 12"
   end
 
   def sign_in_as_admin
@@ -25,5 +31,16 @@ feature "New user, new sign-up", type: :feature,  js: false  do
     fill_in "Password", with: "password"
 
     click_on "Sign in"
+  end
+
+  def fill_in_order_details
+    fill_in "Date", with: (Date.current + 3.days).strftime
+
+    page.find('.add').trigger('click')
+
+    fill_in "Quantity", with: 12
+    select "White sour"
+
+    click_on 'Create Order'
   end
 end
