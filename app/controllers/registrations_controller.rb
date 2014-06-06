13435afce788ec888260f4d55f004f0d282dcac5
Alex Frost <alex.frost.dev@gmail.com>
2014-06-06 10:36:48 +0100

    fix bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,7 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_subscriber_sub_path(resource)
+    edit_subscriber_subs_path(resource)
   end
 
   private
