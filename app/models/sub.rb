4783229f93339246d4cacf6f342a7cbb7fc1651f
Alex Frost <alex.frost.dev@gmail.com>
2014-06-06 11:26:06 +0100

    more specs and refactor

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -9,7 +9,7 @@ class Sub
 
   def cancel
     customer = Stripe::Customer.retrieve(@subscriber.stripe_customer_id)
-    customer.cancel_subscription()
+    customer.cancel_subscription
 
     @subscriber.update_attributes(active_sub: nil)
 
@@ -21,10 +21,19 @@ class Sub
   end
 
   def add(stripe_token)
-    if add_stripe_plan(stripe_token)
+    stripe_customer = add_stripe_plan(stripe_token)
+
+    if stripe_customer
       Notifier.new_sub(@subscriber)
+
+      @subscriber.update ({
+        stripe_customer_id: stripe_customer.id,
+        active_sub: @subscriber.bread_types.size
+      })
+
       true
     else
+      errors.add :base, "Our system is temporarily unable to process credit cards."
       false
     end
   end
@@ -32,22 +41,14 @@ class Sub
   private
 
   def add_stripe_plan(stripe_token)
-    stripe_customer = Stripe::Customer.create(
+    Stripe::Customer.create(
       :email => @subscriber.email,
       :card  => stripe_token,
       :plan  => "weekly-bread-#{@subscriber.bread_types.size}",
     )
 
-    @subscriber.update ({
-      stripe_customer_id: stripe_customer.id,
-      active_sub: @subscriber.bread_types.size
-    })
-
   rescue Stripe::APIError => e
     Rails.logger.error "Stripe Authentication error while creating user: #{e.message}"
-
-    errors.add :base, "Our system is temporarily unable to process credit cards."
-
     false
   end
 
--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -2,10 +2,10 @@ describe Sub do
   subject { Sub.new(subscriber)}
 
   describe '#add' do
-    context 'with no current sub' do
-      let(:subscriber) { create :subscriber }
-      let(:stripe_customer) { double(id: "customer_id") }
+    let(:subscriber) { create :subscriber }
+    let(:stripe_customer) { double(id: "customer_id") }
 
+    context 'with no current sub' do
 
       before do
         create :subscriber_item, subscriber: subscriber
@@ -18,9 +18,54 @@ describe Sub do
         subject.add('strip_token')
       end
 
-      it 'sets the subscriber\'s stripe id' do
+      it 'sets the subscriber\'s stripe id and active sub' do
         subject.add('stripe_token')
+
         expect(subscriber.stripe_customer_id).to eq('customer_id')
+        expect(subscriber.active_sub).to eq(1)
+      end
+
+      it 'returns true' do
+        expect(subject.add('strip_token')).to be_true
+      end
+    end
+
+    context 'with no current sub but with Stripe::APIError' do
+      before do
+        Stripe::Customer.stub(:create).and_raise(Stripe::APIError)
+      end
+
+      it "adds errors to base" do
+        expect(subject.errors).to be_empty
+
+        subject.add('strip_token')
+
+        expect(subject.errors).to_not be_empty
+      end
+
+      it 'returns false' do
+        expect(subject.add('strip_token')).to be_false
+      end
+    end
+  end
+
+  describe '#cancel' do
+    let(:subscriber) { create :subscriber, :paid }
+    context 'happy path' do
+      let(:stripe_customer) { double(cancel_subscription: true) }
+
+      before do
+        Stripe::Customer.stub(:retrieve).and_return(stripe_customer)
+      end
+
+      it 'updates active_sub to nil' do
+        expect(subscriber.active_sub).to_not be_nil
+        subject.cancel
+        expect(subscriber.active_sub).to be_nil
+      end
+
+      it 'returns true' do
+        expect(subject.cancel).to be_true
       end
     end
   end
