c838c3179ca3cece6d83bd90dc0eebde4f9bd8b9
Alex Frost <alex.frost.dev@gmail.com>
2014-06-10 21:47:20 +0100

    fix CopyRegularOrders to work with DJ

--- lib/copy_regular_orders.rb
+++ lib/copy_regular_orders.rb
@@ -4,9 +4,9 @@ require 'clockwork'
 
 include Clockwork
 
-class CopyRegularOrders
+class CopyRegularOrders < Struct.new(:date)
   def perform
-    Order.where(regular: true).where(date: Date.today).each do |order|
+    Order.where(regular: true).where(date: date..(date + 6.days) ).each do |order|
       new_order = Order.create(wholesale_customer_id: order.wholesale_customer_id, date: (order.date + 21.days), regular: true)
       order.line_items.each do |line_item|
         new_order.line_items.create(bread_type_id: line_item.bread_type_id, quantity: line_item.quantity)
@@ -15,4 +15,4 @@ class CopyRegularOrders
   end
 end
 
-every(7.days, 'Queueing scheduled job', :at => '14:17') { Delayed::Job.enqueue ScheduledJob.perform }
+every(7.days, 'Copy regular orders') { Delayed::Job.enqueue CopyRegularOrders.new(Date.today) }
