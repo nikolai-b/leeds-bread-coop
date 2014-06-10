1f0f61207bcf7108393993c4262c92d7267edf83
Alex Frost <alex.frost.dev@gmail.com>
2014-06-10 18:25:28 +0100

    fix prod report with multiple bread orders

--- app/models/production_report.rb
+++ app/models/production_report.rb
@@ -6,24 +6,46 @@ class ProductionReport
   end
 
   def production
-    BreadType.find_each.map do |bread_type|
-      bread_type.subscribers.delivery_day(@date + 1.day)
+    BreadType.all.each.map do |bread_type|
+      num_of_bread = Subscriber.delivery_day(@date + 1.days).find_each.inject(0) do |sum, subscriber|
+        sum + subscriber.paid_bread_subs.where(id: bread_type.id).count
+      end
+      BreadProduction.new(name: bread_type.name, num: num_of_bread)
     end
   end
 
   def preproduction
-    BreadType.where(sour_dough: true).find_each.map do |bread_type|
-      bread_type.subscribers.delivery_day(@date + 2.days)
+    BreadType.where(sour_dough: true).each.map do |bread_type|
+      num_of_bread = Subscriber.delivery_day(@date + 2.days).find_each.inject(0) do |sum, subscriber|
+        sum + subscriber.paid_bread_subs.where(id: bread_type.id).count
+      end
+      BreadProduction.new(name: bread_type.name, num: num_of_bread)
     end
   end
 
   def ferment
-    BreadType.find_each.map do |bread_type|
-      if bread_type.sour_dough
-        bread_type.subscribers.delivery_day(@date + 3.days)
-      else
-        bread_type.subscribers.delivery_day(@date + 2.days)
-      end
+    BreadType.all.each.map do |bread_type|
+      num_of_bread = if bread_type.sour_dough
+                       Subscriber.delivery_day(@date + 3.days).find_each.inject(0) do |sum, subscriber|
+                         sum + subscriber.paid_bread_subs.where(id: bread_type.id).count
+                       end
+                     else
+                       Subscriber.delivery_day(@date + 2.days).find_each.inject(0) do |sum, subscriber|
+                         sum + subscriber.paid_bread_subs.where(id: bread_type.id).count
+                       end
+                     end
+      BreadProduction.new(name: bread_type.name, num: num_of_bread)
+    end
+  end
+
+  private
+
+  class BreadProduction
+    include Virtus.value_object
+
+    values do
+      attribute :name, String
+      attribute :num, Integer
     end
   end
 end
--- spec/models/production_report_spec.rb
+++ spec/models/production_report_spec.rb
@@ -6,24 +6,31 @@ describe ProductionReport do
 
     before do
       yeast_bread = create :yeast_bread
+      sour_bread = create :bread_type
       (1..3).each do |add|
         subscriber_yeast = create(:subscriber, :paid, collection_day: (1 + add) )
         create :subscriber_item, subscriber: subscriber_yeast, bread_type: yeast_bread
-        subscriber = create(:subscriber, :paid, collection_day: (1 + add) )
-        create :subscriber_item, subscriber: subscriber
+        subscriber = create(:subscriber, :paid, collection_day: (1 + add), num_paid_subs: 2 )
+        create :subscriber_item, subscriber: subscriber, bread_type: sour_bread
+        create :subscriber_item, subscriber: subscriber, bread_type: sour_bread
       end
     end
 
     it 'production has all breads for tomorrow (n+1)' do
-      expect(subject.production.select{ |p| p.any? }.size ).to eq(2)
+      expect(subject.production.size).to eq(2)
+      expect(subject.production[0].num).to eq(1)
+      expect(subject.production[1].num).to eq(2)
     end
 
     it 'pre-prduction has all sour dough breads for (n+2)' do
-      expect(subject.preproduction.select{ |pp| pp.any? }.size).to eq(1)
+      expect(subject.preproduction.size).to eq(1)
+      expect(subject.preproduction[0].num).to eq(2)
     end
 
     it 'ferment has all sour dough for (n+3) and yeast for (n+2)' do
-      expect(subject.ferment.select{ |f| f.any? }.size).to eq(2)
+      expect(subject.ferment.size).to eq(2)
+      expect(subject.ferment[0].num).to eq(1)
+      expect(subject.ferment[1].num).to eq(2)
     end
   end
 end
