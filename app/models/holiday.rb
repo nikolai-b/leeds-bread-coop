fcecdb6ce952491721b47a7385697d3f1d6f5dfb
Nikolai Berkoff <nikolai.berkoff@blacksquaremedia.com>
2014-08-05 11:10:22 +0100

    add Holiday#will_miss

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,3 +1,10 @@
 class Holiday < ActiveRecord::Base
   belongs_to :subscriber
+
+  def will_miss
+    subscriber_collection_days = subscriber.collection_days
+    (start_date..end_date).inject(0) do |sum, k|
+      1 + sum if subscriber_collection_days.include?(k.wday)
+    end
+  end
 end
