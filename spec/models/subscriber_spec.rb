1f6c920a3df9669b70fe776d6c267d8fad1391ed
Nikolai Berkoff <nikolai.berkoff@gmail.com>
2014-09-10 21:49:30 +0100

    tidy subscriber specs

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -10,9 +10,9 @@ describe Subscriber do
 
     it "adds subscribers (inc. accociations)" do
       params_file = double('params_file').tap {|pf| pf.stub(:path).and_return('data/subscriber_import.csv')}
-      Subscriber.import( params_file )
-      ursula = Subscriber.find_by(email: "ursula@test.com")
-      rachel = Subscriber.find_by(email: "rachel@test.com")
+      described_class.import( params_file )
+      ursula = described_class.find_by(email: "ursula@test.com")
+      rachel = described_class.find_by(email: "rachel@test.com")
 
       expect(ursula.bread_types[0].name).to eq('100% Rye')
       expect(ursula.collection_point.name).to eq('Green Action')
@@ -25,8 +25,6 @@ describe Subscriber do
   end
 
   context "with subscriber items" do
-    subject { create(:subscriber) }
-
     before do
       4.times {create :subscriber_item, subscriber: subject}
       create :subscriber_item, subscriber: subject, paid: false
@@ -39,7 +37,6 @@ describe Subscriber do
     it "returns the num unpaid subscriber items" do
       expect(subject.num_unpaid_subs).to eq(1)
     end
-
   end
 
   describe 'collection_days' do
@@ -62,45 +59,13 @@ describe Subscriber do
       end
 
       it 'excludes the subscribers on holiday' do
-        expect(Subscriber.active_on(Date.tomorrow.beginning_of_week + 18.days).count).to eq 1
+        expect(described_class.active_on(Date.tomorrow.beginning_of_week + 18.days).count).to eq 1
       end
 
       it 'excludes unpaid subscribers' do
         subscriber.subscriber_items[0].update_attribute :paid, false
-        expect(Subscriber.active_on(Date.tomorrow.beginning_of_week + 18.days).count).to eq 0
+        expect(described_class.active_on(Date.tomorrow.beginning_of_week + 18.days).count).to eq 0
       end
     end
   end
-#  describe 'update_collection_day_check' do
-#    let(:collection_day_updated_at) { Date.parse('2014-06-01') }
-#    before do
-#      subject.update_attribute(:collection_day_updated_at, collection_day_updated_at )
-#    end
-#
-#    context 'with no changes'  do
-#      it 'collection_day_updated_at remains unchanged' do
-#        expect(subject.collection_day_updated_at).to eq(collection_day_updated_at)
-#      end
-#    end
-#
-#    context 'with changes to collection_day' do
-#      before do
-#        subject.update({collection_day: 4})
-#      end
-#
-#      it 'then collection_day_updated_at changes' do
-#        expect(subject.collection_day_updated_at).to eq(Date.today)
-#      end
-#    end
-#
-#    context 'with changes to the bread type name' do
-#      before do
-#        subject.update("subscriber_items_attributes"=>{"1402307812703"=>{"bread_type_id"=>"2", "_destroy"=>"false"}})
-#      end
-#
-#      it 'then collection_day_updated_at changes' do
-#        expect(subject.collection_day_updated_at).to eq(Date.today)
-#      end
-#    end
-#  end
 end
