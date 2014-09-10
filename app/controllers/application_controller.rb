330ca283ecc9dff9a7c6ff9de4698c4ddb17aa28
Nikolai Berkoff <nikolai.berkoff@gmail.com>
2014-09-11 00:12:19 +0100

    fix, permit first / last name params, pass form url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def allowed_subscriuber_parms
-    [:name, :address, :phone, :collection_point_id, :quantity, :notes, subscriber_items_attributes: [:bread_type_id, :collection_day, :id, :_destroy] ]
+    [:first_name, :last_name, :address, :phone, :collection_point_id, :quantity, :notes, subscriber_items_attributes: [:bread_type_id, :collection_day, :id, :_destroy] ]
   end
 
 end
