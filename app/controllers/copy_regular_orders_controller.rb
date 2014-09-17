fb4449507f4a9e09186c33996e8b9652d25fd0b7
Nikolai Berkoff <nikolai.berkoff@gmail.com>
2014-09-17 20:41:47 +0100

    simplify copy regular orders controller

--- app/controllers/copy_regular_orders_controller.rb
+++ app/controllers/copy_regular_orders_controller.rb
@@ -3,10 +3,7 @@ class CopyRegularOrdersController < ApplicationController
   skip_before_action :authenticate_admin, only: [:new]
 
   def new
-    if CopyRegularOrder.new.perform == 0
-      head :no_content
-    else
-      render json: {errors: ""}, status: :unprocessable_entity
-    end
+    CopyRegularOrder.perform
+    head :no_content
   end
 end
