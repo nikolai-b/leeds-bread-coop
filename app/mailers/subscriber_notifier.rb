b09cddf33f6ed53fd9ec678c54dcc26920e1b161
Nikolai Berkoff <alex.frost.dev@gmail.com>
2014-09-25 23:03:35 +0100

    mv invoice to Stripe:Api - pending specs

--- app/mailers/subscriber_notifier.rb
+++ app/mailers/subscriber_notifier.rb
@@ -36,35 +36,6 @@ class SubscriberNotifier
                  body: "Someone is disputing a payment on Stipe, best log into the Stipe account to find out more -\n #{charge}")
   end
 
-  class Invoice
-    def initialize(invoice)
-      @invoice = invoice
-    end
-
-    def total
-      (@invoice.total.to_f / 100.0).to_s
-    end
-
-    def amount_due
-      (@invoice.amount_due.to_f / 100.0).to_s
-    end
-
-    def period_start
-      Time.at(@invoice.lines.data[0].period.start).strftime("%d/%m/%Y") #maybe @invoice.period_start
-    end
-
-    def period_end
-      Time.at(@invoice.lines.data[0].period.end).strftime("%d/%m/%Y") #maybe invoice.lines.data[0].period.end
-    end
-
-    def next_payment_attempt
-      Time.at(next_payment_attempt_raw).strftime("%d/%m/%Y")
-    end
-
-    def next_payment_attempt_raw
-      @invoice.next_payment_attempt || Time.new.to_i
-    end
-  end
 
   private
 
--- app/models/stripe_api.rb
+++ app/models/stripe_api.rb
@@ -28,4 +28,33 @@ class StripeAPI
     stripe_dispute(charge)
   end
 
+  class Invoice
+    def initialize(invoice)
+      @invoice = invoice
+    end
+
+    def total
+      (@invoice.total.to_f / 100.0).to_s
+    end
+
+    def amount_due
+      (@invoice.amount_due.to_f / 100.0).to_s
+    end
+
+    def period_start
+      Time.at(@invoice.lines.data[0].period.start).strftime("%d/%m/%Y") #maybe @invoice.period_start
+    end
+
+    def period_end
+      Time.at(@invoice.lines.data[0].period.end).strftime("%d/%m/%Y") #maybe invoice.lines.data[0].period.end
+    end
+
+    def next_payment_attempt
+      Time.at(next_payment_attempt_raw).strftime("%d/%m/%Y")
+    end
+
+    def next_payment_attempt_raw
+      @invoice.next_payment_attempt || Time.new.to_i
+    end
+  end
 end
