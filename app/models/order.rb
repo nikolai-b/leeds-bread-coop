65186dc73e064ebd8c72f950ef43e28a90858286
Nikolai Berkoff <nikolai.berkoff@gmail.com>
2015-08-21 16:32:00 +0100

    Destroy order's line items on destroy

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :wholesale_customer
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   accepts_nested_attributes_for :line_items, allow_destroy: true
   has_many :bread_types, through: :line_items
   scope :future, -> { where('date >= ?', Date.today) }
