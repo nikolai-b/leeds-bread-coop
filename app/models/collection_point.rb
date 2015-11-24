6ea6502230659bc884b4b9368c232fe87eb039d0
Nikolai B <nikolai-b@users.noreply.github.com>
2015-11-24 22:37:28 +0000

    Fix valid collection day changes

--- app/models/collection_point.rb
+++ app/models/collection_point.rb
@@ -6,6 +6,17 @@ class CollectionPoint < ActiveRecord::Base
 
   scope :ordered, -> { order(:name) }
 
+  ValidDay = Struct.new(:id) do
+    def name
+      Date::DAYNAMES[id]
+    end
+  end
+  class_attribute :all_valid_days
+  self.all_valid_days = [
+    ValidDay.new(2),
+    ValidDay.new(4),
+  ].index_by(&:id).freeze
+
   def valid_day_names
     valid_days.map{ |vd| Date::DAYNAMES[vd][0..2] }.join(', ')
   end
@@ -13,10 +24,13 @@ class CollectionPoint < ActiveRecord::Base
   private
 
   def valid_days_in_weekdays
-    errors.add(:valid_days, 'must be Tue or Thu') unless valid_days.reject(&:blank?).all? {|vd| vd == 4 || vd == 2 }
+    unless valid_days.reject(&:blank?).all? {|vd| vd.in? all_valid_days.keys }
+      errors.add(:valid_days,
+                 "must be #{all_valid_days.values.map(&:name).to_sentence(last_word_connector: 'or')}")
+    end
   end
 
   def normalize_valid_days
-    self.valid_days = valid_days.reject(&:blank?).map &:to_i
+    self.valid_days = valid_days.reject(&:blank?).map(&:to_i)
   end
 end
